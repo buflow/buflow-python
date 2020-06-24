150890e06dd884cdb10c691eb0468d94afb91dec
Soheil <ysoheil@gmail.com>
2020-06-24 10:13:31 -0700

    Add initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+Official Buflow Bindings for Python
+===================================
+
+A Python library for Buflow's API.
+
+
+Setup
+-----
+
+You can install this package by using the pip tool and installing:
+
+    $ pip install Buflow
+    
+Or:
+
+    $ easy_install Buflow
+    
+
+Setting up a Buflow Account
+---------------------------
+
+Sign up for Buflow at https://buflow.com
+
+Using the Buflow API
+--------------------
+
+Documentation for the python bindings can be found alongside Buflow's other bindings here:
+
+- https://buflow.com/docs
+
+In the standard documentation, most of the reference pages will have examples in Buflow's official bindings (including Python). Just click on the Python tab to get the relevant documentation.
---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+os.chdir(here)
+
+with open(
+    os.path.join(here, "README.md"), "r", encoding="utf-8"
+) as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="buflow",
+    version="0.0.1",
+    author="Buflow",
+    author_email="support@buflow.com",
+    description="Python bindings for the Buflow API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/buflow/buflow-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+)
